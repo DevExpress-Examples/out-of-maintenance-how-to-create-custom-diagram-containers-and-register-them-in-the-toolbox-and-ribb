b3617e88d7f09fb095034a050429efc0b5baf3da
DevExpressExampleBot <fakeEmail>
2021-09-02 19:01:15 +0300

    Source auto update [skip ci]

--- VB/DXDiagram.CreateCustomContainers/MainWindow.xaml.vb
+++ VB/DXDiagram.CreateCustomContainers/MainWindow.xaml.vb
@@ -5,25 +5,25 @@ Imports System.Linq
 Imports System.Windows
 
 Namespace DXDiagram.CreateCustomContainers
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits Window
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Private Const MyContainersStencilName As String = "MyContainers"
-        Private Shared ReadOnly containerDescriptions() As ContainerShapeDescription
-        Shared Sub New()
-            Dim customContainersDictionary = New ResourceDictionary() With {.Source = New Uri("CustomContainers.xaml", UriKind.Relative)}
-            containerDescriptions = ShapeDescriptions.LoadDescriptionsFromDictionary(customContainersDictionary).OfType(Of ContainerShapeDescription)().ToArray()
-            DiagramContainerGalleryRegistrator.RegisterContainerShapes(containerDescriptions)
-        End Sub
+		Private Const MyContainersStencilName As String = "MyContainers"
+		Private Shared ReadOnly containerDescriptions() As ContainerShapeDescription
+		Shared Sub New()
+			Dim customContainersDictionary = New ResourceDictionary() With {.Source = New Uri("CustomContainers.xaml", UriKind.Relative)}
+			containerDescriptions = ShapeDescriptions.LoadDescriptionsFromDictionary(customContainersDictionary).OfType(Of ContainerShapeDescription)().ToArray()
+			DiagramContainerGalleryRegistrator.RegisterContainerShapes(containerDescriptions)
+		End Sub
 
-        Public Sub New()
-            InitializeComponent()
-            Dim customContainersStencil = DiagramStencil.Create(MyContainersStencilName, "Custom Containers", containerDescriptions)
-            diagramControl.Stencils = New DiagramStencilCollection(DiagramToolboxRegistrator.Stencils.Concat( { customContainersStencil }))
-            diagramControl.SelectedStencils = New StencilCollection(MyContainersStencilName, BasicShapes.StencilId)
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+			Dim customContainersStencil = DiagramStencil.Create(MyContainersStencilName, "Custom Containers", containerDescriptions)
+			diagramControl.Stencils = New DiagramStencilCollection(DiagramToolboxRegistrator.Stencils.Concat( { customContainersStencil }))
+			diagramControl.SelectedStencils = New StencilCollection(MyContainersStencilName, BasicShapes.StencilId)
+		End Sub
+	End Class
 End Namespace
